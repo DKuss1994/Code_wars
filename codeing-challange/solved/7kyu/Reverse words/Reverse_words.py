0516507fce0b9b827ceffcb01fd033d22342f77a
Dennis Kuß <kussdennis@web.de>
2025-10-03 21:06:46 +0200

    Instructions

--- codeing-challange/solved/7kyu/Reverse words/Reverse_words.py
+++ codeing-challange/solved/7kyu/Reverse words/Reverse_words.py
@@ -11,14 +11,12 @@ def reverse_words(txt):
     list_txt = txt.split(" ")
     reserve_list = []
     new_txt = ""
+    list_txt = txt.split(" ")
     for reserve in list_txt:
         reserve_word = reserve[::-1]
         reserve_list.append(reserve_word)
     for reserve_txt in reserve_list:
         new_txt += reserve_txt + " "
-    if txt[-1] != " ":
-        return new_txt[:-1]
-    else:
-        return new_txt
+    return new_txt[:-1]
 
 
