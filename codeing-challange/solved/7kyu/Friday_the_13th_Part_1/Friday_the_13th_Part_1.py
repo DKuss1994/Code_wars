d5b97351f5c508d0e890d08f0eb0dd1cfdb96699
Dennis Kuß <kussdennis@web.de>
2025-10-13 12:29:46 +0200

    Friday kyu 7

---
+++ codeing-challange/solved/7kyu/Friday_the_13th_Part_1/Friday_the_13th_Part_1.py
@@ -0,0 +1,20 @@
+# Friday the 13th Part 1 (7 kyu)
+
+# https://www.codewars.com/kata/5925acf31a9825d616000e74
+
+# Sprache: Python
+# Datum: 2025-10-09
+
+# t's Friday the 13th, and Jason is ready for his first killing spree!
+#
+# Create a function, killcount, that accepts two arguments: an array of array pairs
+# (the conselor's name and intelligence - ["Chad", 2]) and an integer representing Jason's intellegence.
+
+
+# Skilled: IMit mehreren variabeln in einer for Schleife zu arbeiten
+def kill_count(counselors, jason):
+    jason_kill_list =[]
+    for name,smart in counselors:
+        if smart<jason:
+            jason_kill_list.append(name)
+    return jason_kill_list
