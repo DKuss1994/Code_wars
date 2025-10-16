7fa6ec2aaa29fbe27f0da9a2f7db3aa0215cb0d4
Dennis Kuß <kussdennis@web.de>
2025-10-16 19:34:49 +0200

    new challange

--- codeing-challange/solved/8 kyu/Remove_First_and_Last_Character/Remove_First_and_Last_Character.py
+++ codeing-challange/solved/8 kyu/Remove_First_and_Last_Character/Remove_First_and_Last_Character.py
@@ -1,2 +1,2 @@
 def remove_char(s):
-    #your code here
+    return s[1:len(s)-1]
--- codeing-challange/solved/8 kyu/Remove_First_and_Last_Character/test_Remove_First_and_Last_Character.py
+++ codeing-challange/solved/8 kyu/Remove_First_and_Last_Character/test_Remove_First_and_Last_Character.py
@@ -2,8 +2,10 @@ import unittest
 from Remove_First_and_Last_Character import remove_char
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_hallo(self):
+        self.assertEqual(remove_char("Hallo"), "all")
+    def test_ab(self):
+        self.assertEqual(remove_char("ab"), "")
 
 
 if __name__ == '__main__':
