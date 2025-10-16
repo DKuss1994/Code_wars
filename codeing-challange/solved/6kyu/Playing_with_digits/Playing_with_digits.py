61795cb76a1f581d1c650470a6481c902c2060e0
Dennis Kuß <kussdennis@web.de>
2025-10-16 11:18:49 +0200

    Playing with digits

---
+++ codeing-challange/solved/6kyu/Playing_with_digits/Playing_with_digits.py
@@ -0,0 +1,17 @@
+def dig_pow(n, p):
+    value_x = 0
+    n_list = []
+    n_str = str(n)
+
+#Hier werden die zahlen separiert in eine Liste gepackt.
+    for index in range(len(n_str)):
+        n_list.append(int(n_str[index]))
+    for zahl in n_list:
+        value_x += zahl ** p
+    p += 1
+    if value_x % n == 0:
+        k = value_x / n
+        return int(k)
+    else:
+        return -1
+
---
+++ codeing-challange/solved/6kyu/Playing_with_digits/test_Playing_with_digits.py
@@ -0,0 +1,10 @@
+import unittest
+from Playing_with_digits import dig_pow
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
