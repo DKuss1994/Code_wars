876aa7daf923039b6ef6b360da026171a5beccb1
Dennis Kuß <kussdennis@web.de>
2025-10-21 12:02:24 +0200

    Street Fighter 2 - Character Selection Patr 2 (5 kyu) test

--- codeing-challange/solved/5kyu/test_Street_Fighter_2_Character_Selection_Part_2.py
+++ codeing-challange/solved/5kyu/test_Street_Fighter_2_Character_Selection_Part_2.py
@@ -3,10 +3,17 @@ from Street_Fighter_2_Character_Selection_Part_2 import super_street_fighter_sel
 
 
 class MyTestCase(unittest.TestCase):
-    fighter = ["", "ryu"]
 
     def test_something(self):
-        self.assertEqual(super_street_fighter_selection(), False)  # add assertion here
+        fighters = [
+            ["", "Ryu", "E.Honda", "Blanka", "Guile", ""],
+            ["Balrog", "Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat"],
+            ["Vega", "T.Hawk", "Fei Long", "Deejay", "Cammy", "M.Bison"]
+        ]
+        position = (0, 1)  # start on Ryu
+        moves = ["left", "left", "down", "right"]
+        expected = ["Guile", "Blanka", "Zangief", "Dhalsim"]
+        self.assertEqual(super_street_fighter_selection(fighters,position,moves), expected)  # add assertion here
 
 
 if __name__ == '__main__':
